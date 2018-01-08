b23c28de075f677c106e6c8bce2c385bef849794
GasselinM <m.gasselin@hotmail.fr>
2018-01-08 21:14:06 +0100

    Prend en compte le dernier ID si table déja existante

--- OneD_v2.py
+++ OneD_v2.py
@@ -9,7 +9,7 @@ dsn_database = "postgres"            # e.g. "compose"
 dsn_hostname = "localhost" # e.g.: "aws-us-east-1-portal.4.dblayer.com"
 dsn_port = "5433"                 # e.g. 11101 
 dsn_uid = "postgres"        # e.g. "admin"
-dsn_pwd = "polo_1111"      # e.g. "xxx"
+dsn_pwd = "polo_111"      # e.g. "xxx"
 try:
     conn_string = "host="+dsn_hostname+" port="+dsn_port+" dbname="+dsn_database+" user="+dsn_uid+" password="+dsn_pwd
     print("Connecting to database\n  ->%s" % (conn_string))
@@ -26,9 +26,21 @@ cursor = conn.cursor()
 
 #cursor.execute("DROP TABLE IF EXISTS twitter_OD2")
 cursor.execute("CREATE TABLE IF NOT EXISTS twitter_OD2 (Numero_mesure int, Nombre_de_follower int, Date date, Heure varchar(8))")
-Nmesure = 1
 """cursor.execute("votre_date date NOT NULL DEFAULT '0000-00-00'")
 cursor.execute("heure time NOT NULL DEFAULT '00:00:00'")"""
+
+try:
+    cursor.execute("SELECT MAX(numero_mesure) FROM twitter_OD2")
+    Nmesure = cursor.fetchall()[0][0] + 1
+    print("Nmesure Suite")
+except:
+    Sample=1
+    print("Nmesure à 0")
+
+
+#cursor.execute("IF EXISTS (SELECT MAX(numero_mesure) FROM twitter_OD2)")
+#cursor.execute("(SELECT MAX(numero_mesure) FROM twitter_OD2) IF EXISTS twitter_OD2")
+#Nmesure = cursor.fetchall()[0][0] + 1
 
 
 for i in range (5):
